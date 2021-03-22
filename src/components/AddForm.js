0a71fa090ed53c741fc67db2a2ee4459b977010d
Miu <miu@Mius-MacBook-Pro.local>
2021-03-22 09:26:37 -0700

    assignment done'

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,30 +1,38 @@
-import React, { useRef } from 'react'
-import { connect } from 'react-redux'
+import React, { useRef } from "react";
+import { connect, useSelector, useDispatch } from "react-redux";
 
-const AddForm = ({ newToDo }) => {
-  const refContainer = useRef(null)
+const AddForm = (props) => {
+  const refContainer = useRef(null);
+  // const newToDo = useSelector((state) => state.newToDo); //same as mapStateToProps
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    newToDo({ content: refContainer.current.value })
-    refContainer.current.value = ''
-  }
+    e.preventDefault();
+    newToDo({ content: refContainer.current.value });
+    refContainer.current.value = "";
+  };
+
+  const newToDo = (content) => {
+    return dispatch({ type: "NEW_TO_DO", payload: content });
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Add new todo:</label>
-        <input type='text' ref={refContainer} />
+        <input type="text" ref={refContainer} />
       </form>
     </div>
-  )
-}
+  );
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    newToDo: (content) => {
-      dispatch({ type: 'NEW_TO_DO', payload: content })
-    },
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     newToDo: (content) => {
+//       dispatch({ type: "NEW_TO_DO", payload: content });
+//     },
+//   };
+// };
 
-export default connect(null, mapDispatchToProps)(AddForm)
+export default AddForm;
